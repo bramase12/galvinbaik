fc242d26e7b912884340ef27c7c5c46838aad488
panjollllllllll <ivannideknjoll@gmail.com>
2024-11-04 12:30:30 +0700

    gim2konsol

---
+++ produk.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produk</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<header>
+    <?php  include_once 'navbar/header.html'; ?>
+</header>
+<body>
+    <?php  include_once 'navbar/navbar.html'; ?>
+</body>
+<footer>
+    <?php  include_once 'navbar/footer.html'; ?>
+</footer>
+</html>
