304275df7e7d3c0ab8c0ef3f30d0458e9dcb5b3c
bramasetio <bramaforeverybody@gmail.com>
2026-02-22 15:17:37 +0700

    fix u again

--- api/server/server.php
+++ api/server/server.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'b0umeuwgewtekd1ruqna-mysql.services.clever-cloud.com';
-$db = 'b0umeuwgewtekd1ruqna';
-$user = 'ucdsh8ex7hull0na'; 
-$pass = 'mteCuHDHDtZ9Utyz4UaZ'; 
+$host = 'm8r6rh.h.filess.io';
+$db = 'fufastore_cottonpen';
+$user = 'fufastore_cottonpen'; 
+$pass = 'e4b455cea48e9a519548228aa20d403642e94e4c'; 
 
 $conn = new mysqli($host, $user, $pass, $db);
 
